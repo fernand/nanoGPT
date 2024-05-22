605332d6c81d54d7d80ff58387a18e3bf147293d
Fernand Pajot <fernand@epigram.me>
2024-05-22 12:02:49 -0600

    Single GPU training, not a big loss without torch.compile

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -9,7 +9,9 @@ wandb_run_name='gpt2-124M'
 # these make the total batch size be ~0.5M (491,520)
 batch_size = 16
 block_size = 1024
-gradient_accumulation_steps = 15 * 2
+gradient_accumulation_steps = 30
+
+compile = False
 
 # this makes total number of tokens be 300B
 max_iters = 600000
@@ -18,7 +20,7 @@ lr_decay_iters = 600000
 # eval stuff
 eval_interval = 1000
 eval_iters = 200
-log_interval = 10
+log_interval = 1
 
 # weight decay
 weight_decay = 1e-1
