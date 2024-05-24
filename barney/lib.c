c4fb6a9adc88d2ceae89df882461304a962aa1db
Fernand Pajot <fernand@epigram.me>
2024-05-24 15:21:56 -0600

    FMA is slower than mul and add on Zen3 I guess.

--- barney/lib.c
+++ barney/lib.c
@@ -3,8 +3,6 @@
 #define DIM 768
 #define DIMH 64
 
-#define max(a,b) ((a)>(b) ? (a) : (b))
-
 inline float hsum128(__m128 x)
 {
     x = _mm_add_ps(x, _mm_movehl_ps(x, x));
@@ -34,9 +32,11 @@ void expert_forward(float *x, float *e1, float *e2, float *xo)
         {
             __m256 v1 = _mm256_loadu_ps(&x[i]);
             __m256 v2 = _mm256_loadu_ps(&e1[i]);
-            sum = _mm256_fmadd_ps(v1, v2, sum);
+            __m256 prod = _mm256_mul_ps(v1, v2);
+            __m256 max_val = _mm256_max_ps(prod, zero);
+            sum = _mm256_add_ps(sum, max_val);
         }
-        act[j] = max(0, hsum(sum));
+        act[j] = hsum(sum);
         e1 += DIM;
     }
     for (int j = 0; j < DIM; j++)
@@ -46,7 +46,8 @@ void expert_forward(float *x, float *e1, float *e2, float *xo)
         {
             __m256 v1 = _mm256_loadu_ps(&act[i]);
             __m256 v2 = _mm256_loadu_ps(&e2[i]);
-            sum = _mm256_fmadd_ps(v1, v2, sum);
+            __m256 prod = _mm256_mul_ps(v1, v2);
+            sum = _mm256_add_ps(sum, prod);
         }
         xo[j] = hsum(sum);
         e2 += DIMH;
