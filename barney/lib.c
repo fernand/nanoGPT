a49e933288939341dc07a6652856be53d96f910d
Fernand Pajot <fernand@epigram.me>
2024-05-24 13:43:01 -0600

    working forward.

--- barney/lib.c
+++ barney/lib.c
@@ -1,23 +1,27 @@
 #define DIM 768
 #define DIMH 64
 
-#define max(a,b) ((a)>(b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
-// Input will be X(batch, seq, dim)
-// Each expert inputs will be E1(dim, 64) and E2(64, dim)
-
-float dot(float *a, float *b, int dim)
+/*
+ * x is a single token of shape (DIM,)
+ * e1 is a row oriented matrix of shape (DIM, DIMH)
+ * e2 is a row oriented matrix of shape (DIMH, DIM)
+ * xo points to pre-allocated memory of shape (DIM,)
+ */
+void expert_forward(float *x, float *e1, float *e2, float *xo)
 {
-    float result = 0;
-    for (int i = 0; i < dim; ++i)
-        result += a[i] * b[i];
-    return result;
-}
-
-float dotrelu(float *a, float *b, int dim)
-{
-    float result = 0;
-    for (int i = 0; i < dim; ++i)
-        result += max(0.0f, a[i] * b[i]);
-    return result;
+    float tmp[DIMH] = {0};
+    for (int j = 0; j < DIMH; j++)
+    {
+        for (int i=0; i < DIM; i++)
+            tmp[j] += max(0, e1[i] * x[i]);
+        e1 += DIM;
+    }
+    for (int j = 0; j < DIM; j++)
+    {
+        for (int i=0; i < DIMH; i++)
+            xo[j] += e2[i] * tmp[i];
+        e2 += DIMH;
+    }
 }
--- barney/wrapper.py
+++ barney/wrapper.py
@@ -1,9 +1,20 @@
 import ctypes as ct
 
 import torch
+import torch.nn.functional as F
 
 def ptr(A: torch.Tensor) -> ct.c_void_p:
     assert A is not None
     return ct.c_void_p(A.data.data_ptr())
 
 lib = ct.cdll.LoadLibrary('/home/fernand/nanoGPT/barney/lib.so')
+
+X = torch.rand((768,))
+E1 = torch.rand((64, 768))
+E2 = torch.rand((768, 64))
+Xo = torch.zeros((768,))
+
+lib.expert_forward(ptr(X), ptr(E1), ptr(E2), ptr(Xo))
+
+Xo_ref = E2.matmul(F.relu(E1.matmul(X)))
+print(Xo[0], Xo_ref[0])
